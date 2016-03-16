c57342eb885b90d9891aa1915897a13613b8b890
Juan Gonzalez <juan-carlos.gonzalez-chacon@hpe.com>
2016-03-16 14:48:04 -0700

    Context Feature

--- lib/topology_lib_scapy/library.py
+++ lib/topology_lib_scapy/library.py
@@ -24,8 +24,9 @@ from __future__ import print_function, division
 
 # Add your library functions here.
 
+class ContextManager(object):
 
-class Scapy:
+class Scapy(ContextManager):
 
     def __init__(self, enode):
         self.enode = enode
