50bf9b9afb4584868bece738ad18e751bb9d6b3e
Juan Gonzalez <juan-carlos.gonzalez-chacon@hpe.com>
2016-03-21 09:29:45 -0700

    COntext

--- lib/topology_lib_scapy/library.py
+++ lib/topology_lib_scapy/library.py
@@ -34,6 +34,7 @@ class CLI:
         self.initial_prompt = self.enode._shells['bash']._prompt
         self.enode._shells['bash']._prompt = '>>> '
         self.enode._shells['bash'].send_command('scapy')
+        self.enode._shells['bash'].get_response()
         return self
 
     def __exit__(self, type, value, traceback):
