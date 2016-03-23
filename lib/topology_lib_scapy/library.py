60e100ed68df6a3702e8a0ec012e39fbc89f42cb
Juan Gonzalez <juan-carlos.gonzalez-chacon@hpe.com>
2016-03-23 13:04:03 -0700

    Addded load protocols

--- lib/topology_lib_scapy/library.py
+++ lib/topology_lib_scapy/library.py
@@ -22,6 +22,7 @@ topology_lib_scapy communication library implementation.
 from __future__ import unicode_literals, absolute_import
 from __future__ import print_function, division
 from __future__ import with_statement
+from urllib2 import Request
 #import urllib2
 
 
